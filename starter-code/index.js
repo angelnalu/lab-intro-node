8df6db507d1b66fcf3c7cbc2cd3082ce7e2f2831
Nayhara Luiza <nlm.nalu@yahoo.com.br>
2020-03-16 16:03:31 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(x) {
+    this.items.push(x);
+    this.items.sort( (a,b) => a - b );
+    this.length ++;
+    }
+  
+  
 
-  max() {}
+  get(pos) {
+    if (pos <= this.length){
+        return this.items[pos];
+      }
+      throw new Error("OutOfBounds");
+  }
 
-  min() {}
 
-  sum() {}
+  max() {
+    if (!this.items.length)
+          throw new Error("EmptySortedList");
 
-  avg() {}
+        return this.items[this.items.length - 1];
+  }
+
+  min() {
+    if (this.items.length)
+          return this.items[0];
+
+      throw new Error("EmptySortedList");
+  }
+
+  sum() {
+    return this.items.reduce((acc, val) => { return acc + val }, 0);
+  }
+
+  avg() {
+    if (this.items.length)
+        return this.sum() / this.length;
+
+      throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
